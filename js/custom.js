a0b16894fea9bb09270f3b34f74fbab980c6627b
Mostafa Emad Gomaa <42499279+Mostafaemad228@users.noreply.github.com>
2024-05-13 14:57:07 +0300

    makka code

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+
+  // /////////////////////////////////////////
+  window.addEventListener("scroll" , function () {    
+    var navbar =  document.querySelector("nav");
+    navbar.classList.toggle("sticky",window.scrollY >50);
+  })
+  // ///////////////////////////////////////////////////////////// ///////////////////////
+ 
+  
+// var myHeader = document.getElementById("header");
+// var i = 0;
+// var headerImages =[
+//     "images/banner/slider-1.jpg",
+//     "images/banner/slider-2.jpg",
+//     "images/banner/slider-3.jpg"
+// ];
+
+//  function slideShow() {
+//     myHeader.style.backgroundImage = "url("+headerImages[i]+")"
+
+//     if ( i < headerImages.length - 1) {
+//         i++
+//     }
+//     else{
+//         i = 0
+//     }
+//     setTimeout("slideShow()",3000);
+//  }
+//  slideShow()
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
